3949f689a187da0e75864fcbf902fdb8fa8a7438
Chaewon Kim <kcwchae@gmail.com>
2024-04-15 21:35:34 +0900

    doing step1

--- Help/guide/tutorial/Step1/tutorial.cxx
+++ Help/guide/tutorial/Step1/tutorial.cxx
@@ -1,13 +1,14 @@
 // A simple program that computes the square root of a number
 #include <cmath>
-#include <cstdlib> // TODO 5: Remove this line
 #include <iostream>
 #include <string>
 
 // TODO 11: Include TutorialConfig.h
+#define Tutorial_VERSION_MAJOR @Tutorial_VERSION_MAJOR@
+#define Tutorial_VERSION_MINOR @Tutorial_VERSION_MINOR@
 
 int main(int argc, char* argv[])
-{
+{   
   if (argc < 2) {
     // TODO 12: Create a print statement using Tutorial_VERSION_MAJOR
     //          and Tutorial_VERSION_MINOR
@@ -17,7 +18,7 @@ int main(int argc, char* argv[])
 
   // convert input to double
   // TODO 4: Replace atof(argv[1]) with std::stod(argv[1])
-  const double inputValue = atof(argv[1]);
+  const double inputValue = std::stod(argv[1]);
 
   // calculate square root
   const double outputValue = sqrt(inputValue);
